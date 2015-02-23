dbbb9c152bd9ac6bed422028afa7e2066e8909b0
Pietro Di Bello <orione-team+dip@xpeppers.com>
2015-02-23 16:43:26 +0100

    test 5 completed passes tests

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -30,11 +30,34 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testSpare() {
-		g.roll(5);
-		g.roll(5); // spare
+		rollSpare();
 		g.roll(3);
 		rollMany(17,0);
 		assertEquals(16, g.score());
+	}
+	
+	@Test
+	public void testStrike(){
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16,0);
+		assertEquals(24, g.score());
+	}
+	
+	@Test
+	public void testPerfectGame(){
+		rollMany(12,10);
+		assertEquals(300, g.score());
+	}
+
+	private void rollStrike() {
+		g.roll(10);
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 
 }
--- src/Game.java
+++ src/Game.java
@@ -10,17 +10,37 @@ public class Game {
 	
 	public int score(){
 		int score = 0;
-		int i=0;
+		int frameIndex=0;
 		for (int frame = 0; frame < 10; frame++){
-			if(rolls[i] + rolls[i+1] == 10){ // spare
-				score += 10 + rolls[i+2];
-				i += 2;
+			if(isStrike(frameIndex)) {
+				score += 10 + strikeBonus(frameIndex);
+				frameIndex++;
+			}
+			else if(isSpare(frameIndex)){
+				score += 10 + spareBonus(frameIndex);
+				frameIndex += 2;
 			}
 			else {
-				score += rolls[i] + rolls[i+1];
-				i += 2;
+				score += rolls[frameIndex] + rolls[frameIndex+1];
+				frameIndex += 2;
 			}
 		}
 		return score;
+	}
+
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+
+	private int spareBonus(int frameIndex) {
+		return rolls[frameIndex+2];
+	}
+
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex+1] + spareBonus(frameIndex);
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
 	}
 }
